435360f40ad7867ac95c4aac833fb832d55526fd
Gustavo <gstvcaixeta@gmail.com>
2025-06-07 12:52:07 -0300

    feat:selecionar contrato funcionando e emitindo

--- src/app/components/lista-contratos/lista-contratos.component.ts
+++ src/app/components/lista-contratos/lista-contratos.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Cliente } from '../../models/cliente.interface';
 import { CommonModule } from '@angular/common';
 import { Contrato } from '../../models/contract.interface';
+import { MOCK_CONTRATOS } from '../../../data/mock';
 
 @Component({
   selector: 'lista-contratos',
@@ -10,15 +11,22 @@ import { Contrato } from '../../models/contract.interface';
   templateUrl: './lista-contratos.component.html',
   styleUrl: './lista-contratos.component.css'
 })
-export class ListaContratosComponent {
+export class ListaContratosComponent implements OnChanges{
 
   @Input() cliente: Cliente | null = null;
-  @Input() contracts: Contrato[] = [];
-  @Input() selectedContract: Contrato | null = null;
+  contratosCliente: Contrato[] = [];
+  todosContratos = MOCK_CONTRATOS;
+  contratoSelecionado: Contrato | null = null;
+
+  @Output() novoContratoSelecionado = new EventEmitter<Contrato>; // Substitua 'any' pelo tipo de evento que você deseja emitir
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.contratosCliente = this.todosContratos.filter(contrato => contrato.client_id === this.cliente?.cliente_id);
+  }
 
   onSelectContract(contract: Contrato) {
-    // Implemente a lógica de seleção conforme necessário
-    // Exemplo: emitir evento para o componente pai
+    this.contratoSelecionado = contract;
+    this.novoContratoSelecionado.emit(contract);
   }
 
 }
